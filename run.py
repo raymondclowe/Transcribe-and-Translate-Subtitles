cc8fee69cd604dbd572fb0f6bbe1740cfe4cc7fd
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-01-13 17:18:03 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1454,7 +1454,6 @@ def handle_inputs(
                         text_file.write(f"{text_combine}\n")
                         subtitles_file.write(f"{idx}\n{timestamp}{text_combine}\n\n")
                         idx += 1
-            del text_combine
             del save_text
             del save_timestamps
         print("\nSave completed for timestamps, transcribe text, and original language subtitles.")
