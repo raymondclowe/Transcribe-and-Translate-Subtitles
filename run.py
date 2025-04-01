c11c0238c1146be91bf189a3073d9d33bd08989f
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-01 10:12:45 +0800

    Update run.py

--- run.py
+++ run.py
@@ -701,6 +701,12 @@ def format_time(seconds):
     return f"{hours:02}:{minutes:02}:{seconds:02}.{milliseconds:03}"
 
 
+def normalize_to_int16(audio):
+    max_val = np.max(np.abs(audio.astype(np.float32)))
+    scaling_factor = 32767.0 / max_val if max_val > 0 else 1.0
+    return (audio * float(scaling_factor)).astype(np.int16)
+
+
 def handle_inputs(
         file_path_input,
         task,
@@ -1036,12 +1042,14 @@ def handle_inputs(
                 de_audio = np.array(AudioSegment.from_file(f"./Cache/{file_name}_{denoiser_name}.wav").set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
                 audio = np.array(AudioSegment.from_file(input_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
                 min_len = min(audio.shape[-1], de_audio.shape[-1])
-                audio = (audio[:min_len] * slider_denoise_factor_minus + de_audio[:min_len] * slider_denoise_factor).clip(min=-32768.0, max=32767.0).astype(np.int16)
+                audio = (audio[:min_len] * slider_denoise_factor_minus + de_audio[:min_len] * slider_denoise_factor).clip(min=-32768.0, max=32767.0).astype(np.int32)
+                audio = normalize_to_int16(audio)
                 del de_audio
                 if vad_type == 3:
                     sf.write(f"./Cache/{file_name}_vad.wav", audio, SAMPLE_RATE, format='WAVEX')
             else:
-                audio = np.array(AudioSegment.from_file(input_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int16)
+                audio = np.array(AudioSegment.from_file(input_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int32)
+                audio = normalize_to_int16(audio)
                 if FIRST_RUN:
                     if denoiser_name == "ZipEnhancer":
                         ort_session_A = onnxruntime.InferenceSession(onnx_model_A, sess_options=session_opts, providers=ORT_Accelerate_Providers, provider_options=provider_options)
@@ -1053,7 +1061,8 @@ def handle_inputs(
                     out_name_A0 = out_name_A[0].name
                     print(f"\nDenoise - Usable Providers: {ort_session_A.get_providers()}")
         else:
-            audio = np.array(AudioSegment.from_file(input_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int16)
+            audio = np.array(AudioSegment.from_file(input_audio).set_channels(1).set_frame_rate(SAMPLE_RATE).get_array_of_samples(), dtype=np.int32)
+            audio = normalize_to_int16(audio)
             if vad_type == 3:
                 sf.write(f"./Cache/{file_name}_vad.wav", audio, SAMPLE_RATE, format='WAVEX')
         if FIRST_RUN:
